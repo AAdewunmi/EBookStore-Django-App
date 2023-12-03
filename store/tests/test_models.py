0f5057fabe8802980ad510df7ce0bbd3eb22fe17
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-12-03 12:32:46 +0000

    Nineteenth Commit: Implement test_products_model_entry function

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
@@ -24,6 +25,13 @@ class TestProductsModel(TestCase):
                                             slug='django-beginners', price='20.00', image='django')
         self.data2 = Product.products.create(category_id=1, title='django advanced', created_by_id=1,
                                              slug='django-advanced', price='20.00', image='django', is_active=False)
+    def test_products_model_entry(self):
+        """
+        Test product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
     
 
 
