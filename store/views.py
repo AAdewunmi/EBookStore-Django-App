89d3ebe3b4fe9489def4ccdd48b50a86ef6b490a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-01-12 15:21:14 +0000

    Fifty Commit: Implement custom object manager

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 def all_products(request):
-    products = Product.objects.all()
+    products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
 def product_detail(request, slug):
