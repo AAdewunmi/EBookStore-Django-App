3927ae8464f1e9a8093528ef44b4a9de51a49df7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-11 13:07:13 +0100

    One-Hundred-Ten Commit: Create skeleton UserBase class

--- account/models.py
+++ account/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+import django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 # Create your models here.
+class UserBase(AbstractBaseUser, PermissionsMixin):
