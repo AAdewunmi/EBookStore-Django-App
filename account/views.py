b4002d4a3e4e418218e9895e1d19441a39089174
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-04-19 16:14:45 +0100

    One-Hundred-Twenty-Nine Commit: Implement User registration form

--- account/views.py
+++ account/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import redirect, render
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes
 from django.http import HttpResponse
 from .tokens import account_activation_token
 # Create your views here.
 from .forms import RegistrationForm
 
 def account_register(request):
-     if request.user.is_authenticated:
-        return redirect('/')
      
      if request.method == 'POST':
         registerForm = RegistrationForm(request.POST)
