50b74c84704d21d844fd855cc4eab291d13739db
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-05-11 11:57:07 +0100

    One Hundred Sixty-Eight Commit: Implement PwdResetConfirmForm class

--- account/forms.py
+++ account/forms.py
@@ -71,6 +71,14 @@ class PwdResetForm(PasswordResetForm):
             raise forms.ValidationError(
                 'Unfortunatley we can not find that email address')
         return email
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+    new_password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
+
 class UserEditForm(forms.ModelForm):
 
     email = forms.EmailField(
