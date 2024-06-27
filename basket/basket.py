db990933231f4d2c5be9ebae454daaf1805b09ef
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-06-27 07:41:34 +0100

    Two Hundred Nine Commit: Refactor basket payment to include postage calculation

--- basket/basket.py
+++ basket/basket.py
@@ -50,6 +50,14 @@ class Basket():
     
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+        
+        if subtotal == 0:
+            shipping = Decimal(0.00)
+        else:
+            shipping = Decimal(11.50)
+
+        total = subtotal + Decimal(shipping)
+        return total
     
     def delete(self, product):
         """
