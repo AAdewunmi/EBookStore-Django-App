b14b8018de78f95614b339daa664192214ca6a2b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-08-09 07:05:54 +0100

    Two Hundred Forty Seven Commit: Implement Customer class

--- account/models.py
+++ account/models.py
@@ -32,20 +32,10 @@ class CustomAccountManager(BaseUserManager):
         user.save()
         return user
    
-class UserBase(AbstractBaseUser, PermissionsMixin):
+class Customer(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    about = models.TextField(_(
-        'about'), max_length=500, blank=True)
-    # Delivery details
-    country = CountryField()
-    phone_number = models.CharField(max_length=15, blank=True)
-    postcode = models.CharField(max_length=12, blank=True)
-    address_line_1 = models.CharField(max_length=150, blank=True)
-    address_line_2 = models.CharField(max_length=150, blank=True)
-    town_city = models.CharField(max_length=150, blank=True)
-    # User Status
+    name = models.CharField(max_length=150)
+    mobile = models.CharField(max_length=20, blank=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
@@ -54,12 +44,20 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name']
+    REQUIRED_FIELDS = ['name']
 
     class Meta:
         verbose_name = "Accounts"
         verbose_name_plural = "Accounts"
 
+    def email_user(self, subject, message):
+        send_mail(
+            subject,
+            message,
+            'l@1.com',
+            [self.email],
+            fail_silently=False,
+        )
+
     def __str__(self):
-        return self.user_name
-    
+        return self.name
