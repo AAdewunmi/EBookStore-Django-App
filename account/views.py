8bcc0c8727664bb2a47ef18e31651d82273f42fa
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-08-14 10:18:21 +0100

    Two Hundred Sixty Five Commit: Implement delete_address() method

--- account/views.py
+++ account/views.py
@@ -115,3 +115,8 @@ def edit_address(request, id):
         address_form = UserAddressForm(instance=address)
     return render(request, "account/dashboard/edit_addresses.html", {"form": address_form})
 
+@login_required
+def delete_address(request, id):
+    address = Address.objects.filter(pk=id, customer=request.user).delete()
+    return redirect("account:addresses")
+
